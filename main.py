07b11ae138fc1c2eb36cdf1e872d7e2bf9f2e4e7
Ismaeel <19865145+IsmaeelAkram@users.noreply.github.com>
2022-10-03 16:01:14 -0400

    sample card details

--- main.py
+++ main.py
@@ -1,8 +1,48 @@
+import json
 import functions_framework
-from flask import Request, Response
+from flask import Request, Response, jsonify
+
+cards = [
+    [
+        {
+            "id": "1eaa989b-9cd1-4641-ae3d-03198519d73c",
+            "name": "card 1",
+            "content": "pfokweropfkpwe",
+        },
+        {
+            "id": "939d3ea3-d506-4c19-a5c2-9b92f3eb7c1c",
+            "name": "card 2 ",
+            "content": "fg[plfp[welfwefewfwef",
+        },
+        {
+            "id": "eba1c8ce-70ad-4fa9-a1b8-fe656377f11f",
+            "name": "card 3",
+            "content": "feiopwkfpwekfopfkwe",
+        },
+    ]
+]
 
 
 @functions_framework.http
 def home(request: Request):
-    text = "Quizlet function v1"
-    return Response(text, mimetype="text/plain")
+    if request.method == "POST":
+        return Response(
+            json.dumps(cards),
+            headers={
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+                "Access-Control-Allow-Headers": "Content-Type",
+                "Access-Control-Max-Age": "3600",
+            },
+        )
+    elif request.method == "GET":
+        text = "Please use POST method"
+        return Response(
+            text,
+            headers={
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+                "Access-Control-Allow-Headers": "Content-Type",
+                "Access-Control-Max-Age": "3600",
+            },
+        )
